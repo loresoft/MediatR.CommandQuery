609c76d856bd63b7cc96b5d4632c97e47dc09f7e
Paul Welter <pwelter@loresoft.com>
2019-03-02 17:10:11 -0600

    add DataContext base handler

---
+++ src/EntityFrameworkCore.CommandQuery/Handlers/DataContextHandlerBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace EntityFrameworkCore.CommandQuery.Handlers
+{
+    public abstract class DataContextHandlerBase<TContext, TRequest, TResponse>
+        : RequestHandlerBase<TRequest, TResponse>
+        where TContext : DbContext
+        where TRequest : IRequest<TResponse>
+    {
+        protected DataContextHandlerBase(ILoggerFactory loggerFactory, TContext dataContext, IMapper mapper)
+            : base(loggerFactory)
+        {
+            DataContext = dataContext ?? throw new ArgumentNullException(nameof(dataContext));
+            Mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        protected TContext DataContext { get; }
+        protected IMapper Mapper { get; }
+    }
+}
--- src/EntityFrameworkCore.CommandQuery/Handlers/EntityDataContextHandlerBase.cs
+++ src/EntityFrameworkCore.CommandQuery/Handlers/EntityDataContextHandlerBase.cs
@@ -12,19 +12,13 @@ using Microsoft.Extensions.Logging;
 namespace EntityFrameworkCore.CommandQuery.Handlers
 {
     public abstract class EntityDataContextHandlerBase<TContext, TEntity, TKey, TReadModel, TRequest, TResponse>
-        : RequestHandlerBase<TRequest, TResponse>
-        where TContext : DbContext
+        : DataContextHandlerBase<TContext, TRequest, TResponse> where TContext : DbContext
         where TEntity : class, IHaveIdentifier<TKey>, new()
         where TRequest : IRequest<TResponse>
     {
-        protected TContext DataContext { get; }
-        protected IMapper Mapper { get; }
-
         protected EntityDataContextHandlerBase(ILoggerFactory loggerFactory, TContext dataContext, IMapper mapper)
-            : base(loggerFactory)
+            : base(loggerFactory, dataContext, mapper)
         {
-            DataContext = dataContext ?? throw new ArgumentNullException(nameof(dataContext));
-            Mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
         }
 
         protected virtual async Task<TReadModel> Read(TKey key, CancellationToken cancellationToken = default(CancellationToken))
