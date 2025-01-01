2b4ae911564a31cf93b8227d8c35b26e02fec2ba
Paul Welter <pwelter@loresoft.com>
2024-12-31 22:52:52 -0600

    switch to Testcontainers

--- test/MediatR.CommandQuery.EntityFrameworkCore.SqlServer.Tests/DatabaseFixture.cs
+++ test/MediatR.CommandQuery.EntityFrameworkCore.SqlServer.Tests/DatabaseFixture.cs
@@ -1,18 +1,52 @@
 using MediatR.CommandQuery.Definitions;
 
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
+
+using Testcontainers.MsSql;
 
 using XUnit.Hosting;
 
 namespace MediatR.CommandQuery.EntityFrameworkCore.SqlServer.Tests;
 
-public class DatabaseFixture : TestApplicationFixture
+public class DatabaseFixture : TestApplicationFixture, IAsyncLifetime
 {
+    private readonly MsSqlContainer _msSqlContainer = new MsSqlBuilder()
+        .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
+        .WithPassword("Bn87bBYhLjYRj%9zRgUc")
+        .Build();
+
+    public async Task InitializeAsync()
+    {
+        await _msSqlContainer.StartAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _msSqlContainer.DisposeAsync();
+    }
+
+
     protected override void ConfigureApplication(HostApplicationBuilder builder)
     {
         base.ConfigureApplication(builder);
+
+        // change database from container default
+        var connectionBuilder = new SqlConnectionStringBuilder(_msSqlContainer.GetConnectionString())
+        {
+            InitialCatalog = "TrackerCommandQuery"
+        };
+
+        // override connection string to use docker container
+        var configurationData = new Dictionary<string, string>
+        {
+            ["ConnectionStrings:Tracker"] = connectionBuilder.ToString()
+        };
+
+        builder.Configuration.AddInMemoryCollection(configurationData);
 
         var services = builder.Services;
 
--- test/MediatR.CommandQuery.MongoDB.Tests/DatabaseFixture.cs
+++ test/MediatR.CommandQuery.MongoDB.Tests/DatabaseFixture.cs
@@ -1,3 +1,4 @@
+
 using MediatR.CommandQuery.Definitions;
 
 using Microsoft.Extensions.Configuration;
@@ -6,16 +7,48 @@ using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 
 using MongoDB.Abstracts;
+using MongoDB.Driver;
+
+using Testcontainers.MongoDb;
 
 using XUnit.Hosting;
 
 namespace MediatR.CommandQuery.MongoDB.Tests;
 
-public class DatabaseFixture : TestApplicationFixture
+public class DatabaseFixture : TestApplicationFixture, IAsyncLifetime
 {
+    private readonly MongoDbContainer _mongoDbContainer = new MongoDbBuilder()
+        .WithUsername(string.Empty)
+        .WithPassword(string.Empty)
+        .Build();
+
+    public async Task InitializeAsync()
+    {
+        await _mongoDbContainer.StartAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _mongoDbContainer.DisposeAsync();
+    }
+
     protected override void ConfigureApplication(HostApplicationBuilder builder)
     {
         base.ConfigureApplication(builder);
+
+        // change database from container default
+        var connectionBuilder = new MongoUrlBuilder(_mongoDbContainer.GetConnectionString())
+        {
+            DatabaseName = "CommandQueryTracker"
+        };
+
+        // override connection string to use docker container
+        var configurationData = new Dictionary<string, string>
+        {
+            ["ConnectionStrings:Tracker"] = connectionBuilder.ToString()
+        };
+
+        builder.Configuration.AddInMemoryCollection(configurationData);
 
         var services = builder.Services;
 
@@ -27,6 +60,7 @@ public class DatabaseFixture : TestApplicationFixture
         {
             var configuration = sp.GetRequiredService<IConfiguration>();
             var connectionString = configuration.GetConnectionString("Tracker");
+
             return MongoFactory.GetDatabaseFromConnectionString(connectionString);
         });
         services.AddSingleton(typeof(IMongoEntityRepository<>), typeof(MongoEntityRepository<>));
